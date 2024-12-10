5ff40c2d1647a831f017582580244bfba7a650e1
Dario <master15820@gmail.com>
2024-12-10 23:16:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Playing Atari-Pong with DQN
+Gif of the gameplay
+<table>
+<tr>
+<td>
+<image src="resources/pong.gif"></image>
+</td>
+</tr>
+</table>
---
+++ lib/double_dqn_loss.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+def calc_loss(batch, net, tgt_net, GAMMA, device="cpu"):
+    states, actions, rewards, dones, next_states = batch
+
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.BoolTensor(dones).to(device)
+
+    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1).type(torch.int64)).squeeze(-1)
+    # the following line selects the action with the max Q-value for the next state 
+    next_state_actions = net(next_states_v).max(1)[1]
+    # then the target network is used to evaluate the chosen action, this mitigates overestimation that happens with DQN
+    next_state_values = tgt_net(next_states_v).gather(1, next_state_actions.unsqueeze(-1)).squeeze(-1)
+    next_state_values[done_mask] = 0.0
+    next_state_values = next_state_values.detach()
+
+    expected_state_action_values = next_state_values * GAMMA + rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
